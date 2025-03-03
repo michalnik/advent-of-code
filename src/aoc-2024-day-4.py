8aa3caf097db037fe289c445da655fb87d56b8a9
Michal Mládek <osvc.04923031@gmail.com>
2025-03-03 18:58:25 +0100

    code polishing, final solution for part one of day 4

--- src/aoc-2024-day-4.py
+++ src/aoc-2024-day-4.py
@@ -7,16 +7,17 @@ from utils import (
     validate_file_path,
     parse_args_run_and_profile,
     StreamOfLines,
-    open_stream_of_lines,
     create_stream_of_lines,
 )
 
 
 CountOfXmas: typing.TypeAlias = int
+SearchBlock: typing.TypeAlias = list[str]
+SearchPattern: typing.TypeAlias = list[tuple[int, str]]
 
 
 class SumCountOfXmas(typing.Protocol):
-    def __call__(self, data: StreamOfLines) -> CountOfXmas: ...
+    def __call__(self, data: typing.Iterator[StreamOfLines]) -> CountOfXmas: ...
 
 
 def find_xmas_horizontal(data: StreamOfLines) -> CountOfXmas:
@@ -29,42 +30,60 @@ def find_xmas_horizontal(data: StreamOfLines) -> CountOfXmas:
     return count
 
 
-def find_xmas_vertical(data: StreamOfLines) -> CountOfXmas:
-    count: int = 10
+def is_interesting_block(block_idx: int, line_idx: int) -> bool:
+    return block_idx <= line_idx
+
+
+def search_block_vertical(search_block: SearchBlock, pattern: SearchPattern) -> CountOfXmas:
+    count_of_xmas: int = 0
+    try:
+        for col_idx in range(len(search_block[0])):
+            if all(
+                search_block[row_idx][col_idx + search[0]].lower() == search[1]
+                for row_idx, search in zip(list(range(4)), pattern)
+            ):
+                count_of_xmas += 1
+    except IndexError:
+        # finished
+        pass
+    return count_of_xmas
+
+
+def find_xmas_other_way(data: StreamOfLines, pattern: SearchPattern) -> CountOfXmas:
+    count: int = 0
+    block_idx: int
+    stack: list[SearchBlock] = [[], [], [], []]
+    for line_idx, line in enumerate(data):
+        for block_idx, search_block in enumerate(stack):
+            if is_interesting_block(block_idx, line_idx):
+                search_block.append(line)
+            if len(search_block) == 4:
+                # forward
+                count += search_block_vertical(search_block, pattern)
+                # backward
+                count += search_block_vertical(search_block, pattern[::-1])
+                stack[block_idx] = []
     return count
 
 
-def find_xmas_diagonal(data: StreamOfLines) -> CountOfXmas:
-    count: int = 20
-    return count
-
-
-def find_xmas_all(data: typing.Iterator[StreamOfLines]) -> CountOfXmas:
+def find_xmas_crazy(data: typing.Iterator[StreamOfLines]) -> CountOfXmas:
     count: int = 0
     count += find_xmas_horizontal(next(data))
-    count += find_xmas_vertical(next(data))
-    count += find_xmas_diagonal(next(data))
+    count += find_xmas_other_way(next(data), [(0, "x"), (0, "m"), (0, "a"), (0, "s")])
+    count += find_xmas_other_way(next(data), [(0, "x"), (1, "m"), (2, "a"), (3, "s")])
+    count += find_xmas_other_way(next(data), [(0, "s"), (1, "a"), (2, "m"), (3, "x")])
     return count
 
 
 def main(args: argparse.Namespace):
     summary_of_count_of_xmas: CountOfXmas
 
+    sum_count_of_xmas: SumCountOfXmas
     match args.mode:
-        case "horizontal" | "vertical" | "diagonal":
-            stream_of_lines: StreamOfLines = open_stream_of_lines(args.file_path)
-            sum_count_of_xmas: SumCountOfXmas
-            match args.mode:
-                case "horizontal":
-                    sum_count_of_xmas = find_xmas_horizontal
-                case "vertical":
-                    sum_count_of_xmas = find_xmas_vertical
-                case _:
-                    # diagonal
-                    sum_count_of_xmas = find_xmas_diagonal
-            summary_of_count_of_xmas = sum_count_of_xmas(stream_of_lines)
         case _:
-            summary_of_count_of_xmas = find_xmas_all(create_stream_of_lines(args.file_path))
+            sum_count_of_xmas = find_xmas_crazy
+
+    summary_of_count_of_xmas = sum_count_of_xmas(create_stream_of_lines(args.file_path))
 
     print(f"Summary of multiplications added by <{args.mode}>: ", summary_of_count_of_xmas)
 
@@ -76,8 +95,8 @@ if __name__ == "__main__":
     parser.add_argument("file_path", type=validate_file_path, help="Existing file path")
     parser.add_argument(
         "--mode",
-        choices=["horizontal", "vertical", "diagonal", "all"],
-        default="all",
+        choices=["crazy"],
+        default="crazy",
         help="Mode of counting",
     )
 
